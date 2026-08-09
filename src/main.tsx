00c9ee45149af18725f47b5f311d7c766c4b713d
Ahmad Gofat <107179247+GOFATgraphics@users.noreply.github.com>
2026-08-09 22:44:24 +0100

    Fix deploy: repair App.tsx template literals; import CSS parts from main

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './index-part2.css'
 import './animations.css'
 import './chat-glow-modes.css'
 
